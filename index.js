6b8c022559104755d5cecb3d1799c2b041a31abb
Steve <sayantanmanna06@gmail.com>
2025-03-06 01:58:38 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const connectDB = require('./db/connect');
+const app= express();
+require('dotenv').config();
+
+app.get("/",(req,res)=>{
+    res.status(200).send("API for ProjectDB")
+})
+
+const port= process.env.PORT ?? 5000;
+
+const start=async()=>{
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, ()=>{
+            console.log(`Server is listening on port ${port}`);
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
